85823728d39cfa4d6e7fed62f20ec1083bc7957e
Viettien <viettienn97@gmail.com>
2018-04-02 18:06:11 +0700

    minifix

--- app/assets/javascripts/channels/request.js
+++ app/assets/javascripts/channels/request.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
       disconnected: function() {},
       received: function(data) {
         if (data.content.blank == null) {
-          $('#list-comment').append('<li>' + '<div class="review-section well">' + '<div class= "row">' + '<div class="col-md-4 user-review">' + '<div class = "bold">' + '<h4>' + data.username + '</h4>' + '</div>' + '<i>' + '</i>' + '</div>' + '<div class="col-md-8">' + '<div class = comment-content>' + data.content + '</div>' + '</div>' + '</div>' + '</div>' + '</li>'
+          $('#list-comment').append('<li>' + '<div class="review-section well">' + '<div class= "row">' + '<div class="col-md-4 user-review">' + '<div class = "bold">' + '<img src ="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" height="40" width="40">&nbsp;' + '<strong>'+data.username +'</strong>' + '</div>' + '<i>' + '</i>' + '</div>' + '<div class="col-md-8">' + '<div class = comment-content>' + data.content + '</div>' + '</div>' + '</div>' + '</div>' + '</li>'
           );
         }
       }
